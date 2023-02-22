62f65d7bff81214f69ae788622a3a2b35b3bec35
auorauh <dr.teasdale.trenton@gmail.com>
2023-02-21 18:25:07 -0600

    v2 update

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var slideIndex = 0;
+let slideIndex = 0;
 carousel();
 
 function carousel() {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
+  let i;
+  let x = document.getElementsByClassName("mySlides");
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
   }
