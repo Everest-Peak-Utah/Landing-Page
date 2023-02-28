487c92c4a9419782ce1241e794745f4ecbbe6281
auorauh <dr.teasdale.trenton@gmail.com>
2023-02-28 17:09:56 -0600

    update title and services menu

--- index.js
+++ index.js
@@ -21,3 +21,12 @@ function areaCoveredClick(){
     document.getElementsByClassName('dropdown')[0].style.border = '2px solid black !important';
   }
 }
+function servicesClick(){
+  let x = document.getElementsByClassName('service-content')[0];
+  if(x.style.maxHeight == '0px'){
+    x.style.maxHeight = '250px';
+  } else {
+    x.style.maxHeight = '0px';
+    document.getElementsByClassName('servicesMenu')[0].style.border = '2px solid black !important';
+  }
+}
